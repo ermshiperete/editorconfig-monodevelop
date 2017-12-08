a04f0608cebc55985d7bca6ddcc3446362ae44af
William Kent <wjk@users.noreply.github.com>
2017-12-08 12:41:58 -0500

    Rewrite and modernize addin
    This commit does two things:
    
    * It extends the StartupHandler class to handle all
      file types, not just text and C# sources, as well as
      support multiple .editorconfig files.
    * I rewrote the project file to use the new-style csproj
      format. I did this mainly because the old project
      wouldn't build on my system.

--- MonoDevelopEditorConfigAddin/StartupHandler.cs
+++ MonoDevelopEditorConfigAddin/StartupHandler.cs
@@ -1,90 +1,75 @@
-﻿// Copyright (c) 2016 SIL International
-// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using EditorConfig.Core;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
 using MonoDevelop.Ide.Gui.Content;
 using MonoDevelop.Projects;
 
 namespace MonoDevelopEditorConfigAddin
 {
-	public class StartupHandler: CommandHandler
-	{
-		private EditorConfigParser _parser;
+    public class StartupHandler : CommandHandler
+    {
+        private EditorConfigParser _parser = new EditorConfigParser();
 
-		protected override void Run()
-		{
-			base.Run();
-			IdeApp.Workspace.SolutionLoaded += OnSolutionLoaded;
-		}
+        protected override void Run()
+        {
+            base.Run();
 
-		private void OnSolutionLoaded (object sender, SolutionEventArgs e)
-		{
-			_parser = new EditorConfigParser();
-			UpdatePolicy(e.Solution, "text/x-csharp", ".cs");
-			UpdatePolicy(e.Solution, "text/plain", "");
-		}
+            _parser = new EditorConfigParser();
+            IdeApp.Workbench.DocumentOpened += OnDocumentOpened;
+        }
 
-		void UpdatePolicy(Solution solution, string mimeType, string extension)
-		{
-			var configuration = _parser.Parse(
-				Path.Combine(solution.BaseDirectory, "FileDoesntHaveToExist" + extension)).First();
-			if (configuration == null || configuration.Properties.Count == 0)
-				return;
+        private void OnDocumentOpened(object sender, DocumentEventArgs e)
+        {
+            Solution solution = e.Document.Project.ParentSolution;
+            string path = Path.Combine(e.Document.Project.BaseDirectory, e.Document.PathRelativeToProject);
+            var configuration = _parser.Parse(path).First();
 
-			var mimeTypes = DesktopService.GetMimeTypeInheritanceChain(mimeType);
-			var oldPolicy = solution.Policies.Get<TextStylePolicy>(mimeTypes);
-			var fileWidth = configuration.MaxLineLength.HasValue ?
-				configuration.MaxLineLength.Value : oldPolicy.FileWidth;
-			var eolMarkerVal = TranslateEndOfLine(configuration.EndOfLine);
-			var eolMarker = eolMarkerVal.HasValue ?
-				eolMarkerVal.Value : oldPolicy.EolMarker;
-			var indentWidthVal = GetIndentWidth(configuration);
-			var indentWidth = indentWidthVal.HasValue ?
-				indentWidthVal.Value : oldPolicy.IndentWidth;
-			var tabWidth = configuration.TabWidth.HasValue ?
-				configuration.TabWidth.Value : oldPolicy.TabWidth;
-			var tabsToSpaces = configuration.IndentStyle.HasValue ?
-				(configuration.IndentStyle.Value == IndentStyle.Space) : oldPolicy.TabsToSpaces;
-			var removeTrailingWhitespace = configuration.TrimTrailingWhitespace.HasValue ?
-				configuration.TrimTrailingWhitespace.Value : oldPolicy.RemoveTrailingWhitespace;
-			var newPolicy = new TextStylePolicy(fileWidth, tabWidth, indentWidth, tabsToSpaces,
-				oldPolicy.NoTabsAfterNonTabs, removeTrailingWhitespace, eolMarker);
-			if (newPolicy != oldPolicy)
-				solution.Policies.Set<TextStylePolicy>(newPolicy, mimeType);
-		}
+            if (configuration == null || configuration.Properties.Count == 0) return;
 
-		private EolMarker? TranslateEndOfLine(EndOfLine? endOfLine)
-		{
-			if (!endOfLine.HasValue)
-				return null;
+            Uri uri = new Uri(path);
+            var mimeType = DesktopService.GetMimeTypeForRoslynLanguage(
+                DesktopService.GetMimeTypeForUri(uri.AbsoluteUri)
+            );
 
-			switch (endOfLine.Value)
-			{
-				case EndOfLine.CR:
-					return EolMarker.Mac;
-				case EndOfLine.CRLF:
-					return EolMarker.Windows;
-				case EndOfLine.LF:
-					return EolMarker.Unix;
-			}
-			return EolMarker.Native;
-		}
+            var oldPolicy = solution.Policies.Get<TextStylePolicy>();
+            var fileWidth = configuration.MaxLineLength ?? oldPolicy.FileWidth;
+            var eolMarkerVal = TranslateEndOfLine(configuration.EndOfLine);
+            var eolMarker = eolMarkerVal ?? oldPolicy.EolMarker;
+            var indentWidthVal = GetIndentWidth(configuration);
+            var indentWidth = indentWidthVal ?? oldPolicy.IndentWidth;
+            var tabWidth = configuration.TabWidth ?? oldPolicy.TabWidth;
+            var removeTrailingWhitespace = configuration.TrimTrailingWhitespace ?? oldPolicy.RemoveTrailingWhitespace;
+            var tabsToSpaces = configuration.IndentStyle.HasValue ? (configuration.IndentStyle == EditorConfig.Core.IndentStyle.Space) : oldPolicy.TabsToSpaces;
 
-		private int? GetIndentWidth(FileConfiguration configuration)
-		{
-			if (configuration.IndentSize == null)
-				return null;
+            var newPolicy = new TextStylePolicy(fileWidth, tabWidth, indentWidth, tabsToSpaces, oldPolicy.NoTabsAfterNonTabs, removeTrailingWhitespace, eolMarker);
+            if (newPolicy != oldPolicy) solution.Policies.Set<TextStylePolicy>(newPolicy, mimeType);
+        }
 
-			if (configuration.IndentSize.UseTabWidth)
-				return configuration.TabWidth;
+        private static EolMarker? TranslateEndOfLine(EndOfLine? eol)
+        {
+            if (!eol.HasValue) return null;
 
-			return configuration.IndentSize.NumberOfColumns;
-		}
+            switch (eol.Value)
+            {
+                case EndOfLine.CR: return EolMarker.Mac;
+                case EndOfLine.LF: return EolMarker.Unix;
+                case EndOfLine.CRLF: return EolMarker.Windows;
+                default: return EolMarker.Native;
+            }
+        }
 
-	}
+        private static int? GetIndentWidth(FileConfiguration config)
+        {
+            if (config.IndentSize == null) return null;
+
+            if (config.IndentSize.UseTabWidth)
+                return config.TabWidth;
+
+            return config.IndentSize.NumberOfColumns;
+        }
+    }
 }
-
