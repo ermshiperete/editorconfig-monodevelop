c8c29f29c33a677a0d2a4e9db73453346bcd4645
William Kent <wjk@users.noreply.github.com>
2017-12-08 20:25:39 -0500

    Rewrite (and reindent) StartupHandler.cs
    This class now works as intended, modulo some
    Visual Studio "features" that are incompatible with
    the .editorconfig standard (as well as Visual Studio
    for Windows, as well as my personal formatting standards).

--- MonoDevelopEditorConfigAddin/StartupHandler.cs
+++ MonoDevelopEditorConfigAddin/StartupHandler.cs
@@ -1,75 +1,101 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using EditorConfig.Core;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Ide;
 using MonoDevelop.Ide.Gui;
 using MonoDevelop.Ide.Gui.Content;
 using MonoDevelop.Projects;
+using MonoDevelop.Core.Logging;
 
 namespace MonoDevelopEditorConfigAddin
 {
-    public class StartupHandler : CommandHandler
-    {
-        private EditorConfigParser _parser;
+	public class StartupHandler : CommandHandler
+	{
+		private EditorConfigParser _parser;
 
-        protected override void Run()
-        {
-            base.Run();
+		protected override void Run()
+		{
+			base.Run();
 
-            _parser = new EditorConfigParser();
-            IdeApp.Workbench.DocumentOpened += OnDocumentOpened;
-        }
+			_parser = new EditorConfigParser();
+			IdeApp.Workbench.DocumentOpened += OnDocumentOpened;
+		}
 
-        private void OnDocumentOpened(object sender, DocumentEventArgs e)
-        {
-            Solution solution = e.Document.Project.ParentSolution;
-            string path = Path.Combine(e.Document.Project.BaseDirectory, e.Document.PathRelativeToProject);
-            var configuration = _parser.Parse(path).First();
+		private void OnDocumentOpened(object sender, DocumentEventArgs e)
+		{
+			ILogger logger = new ConsoleLogger();
 
-            if (configuration == null || configuration.Properties.Count == 0) return;
+			Solution solution = e.Document.Project.ParentSolution;
+			string path = Path.Combine(e.Document.Project.BaseDirectory, e.Document.PathRelativeToProject);
+			logger.Log(LogLevel.Info, $"Getting editorconfig data for path: {path}");
 
-            Uri uri = new Uri(path);
-            var mimeType = DesktopService.GetMimeTypeForRoslynLanguage(
-                DesktopService.GetMimeTypeForUri(uri.AbsoluteUri)
-            );
+			var configuration = _parser.Parse(path).First();
 
-            var oldPolicy = solution.Policies.Get<TextStylePolicy>();
-            var fileWidth = configuration.MaxLineLength ?? oldPolicy.FileWidth;
-            var eolMarkerVal = TranslateEndOfLine(configuration.EndOfLine);
-            var eolMarker = eolMarkerVal ?? oldPolicy.EolMarker;
-            var indentWidthVal = GetIndentWidth(configuration);
-            var indentWidth = indentWidthVal ?? oldPolicy.IndentWidth;
-            var tabWidth = configuration.TabWidth ?? oldPolicy.TabWidth;
-            var removeTrailingWhitespace = configuration.TrimTrailingWhitespace ?? oldPolicy.RemoveTrailingWhitespace;
-            var tabsToSpaces = configuration.IndentStyle.HasValue ? (configuration.IndentStyle == IndentStyle.Space) : oldPolicy.TabsToSpaces;
+			if (configuration == null || configuration.Properties.Count == 0)
+			{
+				logger.Log(LogLevel.Warn, "Found no editorconfig data, returning");
+				return;
+			}
 
-            var newPolicy = new TextStylePolicy(fileWidth, tabWidth, indentWidth, tabsToSpaces, oldPolicy.NoTabsAfterNonTabs, removeTrailingWhitespace, eolMarker);
-            if (newPolicy != oldPolicy) solution.Policies.Set(newPolicy, mimeType);
-        }
+			Uri uri = new Uri(path);
+			var mimeType = DesktopService.GetMimeTypeForUri(uri.AbsoluteUri);
 
-        private static EolMarker? TranslateEndOfLine(EndOfLine? eol)
-        {
-            if (!eol.HasValue) return null;
+			var oldPolicy = solution.Policies.Get<TextStylePolicy>();
+			var fileWidth = configuration.MaxLineLength ?? oldPolicy.FileWidth;
+			var eolMarkerVal = TranslateEndOfLine(configuration.EndOfLine);
+			var eolMarker = eolMarkerVal ?? oldPolicy.EolMarker;
+			var indentWidthVal = GetIndentWidth(configuration);
+			var indentWidth = indentWidthVal ?? oldPolicy.IndentWidth;
+			var tabWidth = configuration.TabWidth ?? oldPolicy.TabWidth;
+			var removeTrailingWhitespace = configuration.TrimTrailingWhitespace ?? oldPolicy.RemoveTrailingWhitespace;
+			var tabsToSpaces = configuration.IndentStyle.HasValue ? (configuration.IndentStyle == IndentStyle.Space) : oldPolicy.TabsToSpaces;
 
-            switch (eol.Value)
-            {
-                case EndOfLine.CR: return EolMarker.Mac;
-                case EndOfLine.LF: return EolMarker.Unix;
-                case EndOfLine.CRLF: return EolMarker.Windows;
-                default: return EolMarker.Native;
-            }
-        }
+			var newPolicy = new TextStylePolicy(fileWidth, indentWidth, indentWidth, tabsToSpaces, oldPolicy.NoTabsAfterNonTabs, removeTrailingWhitespace, eolMarker);
+			if (newPolicy != oldPolicy)
+			{
+				logger.Log(LogLevel.Info, $"Setting editorconfig policy: {DescribePolicy(newPolicy)}");
+				solution.Policies.Set(newPolicy, mimeType);
+			}
+		}
 
-        private static int? GetIndentWidth(FileConfiguration config)
-        {
-            if (config.IndentSize == null) return null;
+		private static EolMarker? TranslateEndOfLine(EndOfLine? eol)
+		{
+			if (!eol.HasValue) return null;
 
-            if (config.IndentSize.UseTabWidth)
-                return config.TabWidth;
+			switch (eol.Value)
+			{
+				case EndOfLine.CR: return EolMarker.Mac;
+				case EndOfLine.LF: return EolMarker.Unix;
+				case EndOfLine.CRLF: return EolMarker.Windows;
+				default: return EolMarker.Native;
+			}
+		}
 
-            return config.IndentSize.NumberOfColumns;
-        }
-    }
+		private static int? GetIndentWidth(FileConfiguration config)
+		{
+			if (config.IndentSize == null) return null;
+
+			if (config.IndentSize.UseTabWidth)
+				return config.TabWidth;
+
+			return config.IndentSize.NumberOfColumns;
+		}
+
+		private static string DescribePolicy(TextStylePolicy policy)
+		{
+			StringBuilder builder = new StringBuilder();
+			builder.Append("{ ");
+
+			builder.AppendFormat("tabWidth={0} ", policy.TabWidth);
+			builder.AppendFormat("indentWidth={0} ", policy.IndentWidth);
+			builder.AppendFormat("tabsToSpaces={0} ", policy.TabsToSpaces);
+			builder.AppendFormat("trimWhitespace={0} ", policy.RemoveTrailingWhitespace);
+
+			builder.Append("}");
+			return builder.ToString();
+		}
+	}
 }
